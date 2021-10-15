0a9577497f8faf4f21adef6b048d2a70895f042c
GREMIAR <70177742+GREMIAR@users.noreply.github.com>
2021-10-15 20:03:17 +0300

    save

--- 2-3Tree/2-3Tree/Code.cs
+++ 2-3Tree/2-3Tree/Code.cs
@@ -8,5 +8,29 @@ namespace _2_3Tree
 {
     class Code
     {
+        string str;
+        int[] code;
+        bool digit;
+        public Code(string str)
+        {
+            this.str = str;
+            digit = Int32.TryParse(str, out code);
+            if (digit)
+            {
+            }
+            else
+            { 
+                foreach(char ch in str)
+                {
+                    code[]
+                }
+                
+            }
+        }
     }
 }
+
+//a aa b 7 15 3
+
+//  a b aa
+// 49 49  49 
